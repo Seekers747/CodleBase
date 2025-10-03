2e72f7e47a2942ff79ec56e0fbb293d50fcda65e
Imraan <imraan.mohamedhoesein@gmail.com>
2025-10-03 11:58:44 +0200

    heb restart button een component gemaakt

---
+++ CodleSimple/Components/Elements/RestartButton.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CodleWeb.Components.Elements;
+
+public partial class RestartButton
+{
+  [Parameter]
+  public EventCallback OnClick { get; set; }
+
+  [Parameter]
+  public bool Disabled { get; set; } = false;
+}
